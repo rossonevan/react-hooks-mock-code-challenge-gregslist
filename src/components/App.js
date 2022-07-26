f1820d7afffac5c13ec39c87415dcb86857ac168
Evan Rosson <rossonevan7281@gmail.com>
2022-07-26 08:36:49 -0500

    first deliverable finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  
+  const [listings, setListings] = useState([]);
+  
+  useEffect( () => {
+    fetch('http://localhost:6001/listings')
+    .then(res => res.json())
+    .then(data => setListings(data))
+
+  }, []) 
+    
+  
+  
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings}/>
     </div>
   );
 }
